46be7d1538d9ad1c6908ef1e8a7a28cc300803f6
theDone <done.muzaffer5@gmail.com>
2025-01-16 21:37:36 +0300

    expo router setup

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,21 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+
+      <Link href={"/sign-in"}>Sign</Link>
+      <Link href={"/explore"}>explore</Link>
+      <Link href={"/profile"}>profile</Link>
+      <Link href={"./properties/1"}>Property</Link>
+
+    </View>
+  );
+}
